0602225ebcd64d6df874cd53a6f52e2c5b6aeb1e
Dima Rymar <dimarymar@gmail.com>
2014-05-15 05:48:03 +0300

    begin set-themes

--- src/Nmkd/MainBundle/Controller/NmkdController.php
+++ src/Nmkd/MainBundle/Controller/NmkdController.php
@@ -5,8 +5,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class NmkdController extends Controller
 {
-    public function dicAction()
+    public function inputAction()
     {
-        echo 1;
+        return $this->render(
+            'NmkdMainBundle:Nmkd:input.html.twig'
+        );
     }
+
+    public function setThemesAction()
+    {
+        $questionStr = $_POST['questions'];
+        $questionArr = explode('<br />', nl2br($questionStr));
+        $questionArr = array_map('trim',$questionArr);
+        $questionArr = array_values(array_filter($questionArr));
+        $this->storage()->set('questions', $questionArr);
+
+        return $this->render(
+            'NmkdMainBundle:Nmkd:setThemes.html.twig', array(
+                'questions' => $questionArr
+            )
+        );
+    }
+
+    protected function storage()
+    {
+        return Container::get('static_storage');
+    }
+
 }
