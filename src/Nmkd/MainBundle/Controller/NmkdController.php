25a24ad6965aa3c4f3de14698e0bfb17ff40e844
Dima Rymar <dimarymar@gmail.com>
2014-05-15 03:57:09 +0300

    login/logout done. Add bootstrap

---
+++ src/Nmkd/MainBundle/Controller/NmkdController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Nmkd\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class NmkdController extends Controller
+{
+    public function dicAction()
+    {
+        echo 1;
+    }
+}
